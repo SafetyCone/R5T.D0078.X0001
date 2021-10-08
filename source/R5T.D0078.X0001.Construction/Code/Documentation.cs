944932b31f6af7326a481a178b7671ac2f9ad8dd
davidcoats <david@getsafetycone.com>
2021-10-08 13:17:01 -0700

    Initial commit of source-stub.

---
+++ source/R5T.D0078.X0001.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.D0078.X0001.Construction
+{
+    /// <summary>
+    /// Construction project for Rivet-specific extensions to the IVisualStudioSolutionFileOperator service..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.D0078.X0001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.D0078.X0001
+{
+    /// <summary>
+    /// Rivet-specific extensions to the IVisualStudioSolutionFileOperator service.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
