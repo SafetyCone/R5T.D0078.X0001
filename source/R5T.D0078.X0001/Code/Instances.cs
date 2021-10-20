9b4d8ca245ac314a109aa158c3f82a5eefb4917b
David Coats <david@getsafetycone.com>
2021-10-20 16:01:57 -0700

    Add IVisualStudioSolutionFileOperator extensions.

---
+++ source/R5T.D0078.X0001/Code/Instances.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.T0059;
+
+
+namespace R5T.D0078.X0001
+{
+    public static class Instances
+    {
+        public static ISolutionFolderName SolutionFolderName { get; } = T0059.SolutionFolderName.Instance;
+    }
+}
---
+++ source/R5T.D0078.X0001/Code/Services/Extensions/IVisualStudioSolutionFileOperatorExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using R5T.D0078;
+
+using Instances = R5T.D0078.X0001.Instances;
+
+
+namespace System
+{
+    public static class IVisualStudioSolutionFileOperatorExtensions
+    {
+        public static Task AddDependencyProjectReference(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePathToModify,
+            string dependencyProjectReferenceFilePathToAdd)
+        {
+            return visualStudioSolutionFileOperator.AddProjectReferences(
+                solutionFilePathToModify,
+                dependencyProjectReferenceFilePathToAdd,
+                Instances.SolutionFolderName.Dependencies());
+        }
+
+        public static Task AddDependencyProjectReferences(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePathToModify,
+            IEnumerable<string> dependencyProjectReferenceFilePathsToAdd)
+        {
+            return visualStudioSolutionFileOperator.AddProjectReferences(
+                solutionFilePathToModify,
+                dependencyProjectReferenceFilePathsToAdd,
+                Instances.SolutionFolderName.Dependencies());
+        }
+    }
+}
