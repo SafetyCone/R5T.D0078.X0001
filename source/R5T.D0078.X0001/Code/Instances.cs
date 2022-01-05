877c305713337a2acedcca56252d779f1b0ee2f0
David Coats <david@getsafetycone.com>
2022-01-05 12:19:48 -0800

    Port changes from E0035.

--- source/R5T.D0078.X0001/Code/Instances.cs
+++ source/R5T.D0078.X0001/Code/Instances.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using R5T.T0040;
+using R5T.T0044;
 using R5T.T0059;
 
 
@@ -7,6 +9,8 @@ namespace R5T.D0078.X0001
 {
     public static class Instances
     {
+        public static IFileSystemOperator FileSystemOperator { get; } = T0044.FileSystemOperator.Instance;
         public static ISolutionFolderName SolutionFolderName { get; } = T0059.SolutionFolderName.Instance;
+        public static ISolutionPathsOperator SolutionPathsOperator { get; } = T0040.SolutionPathsOperator.Instance;
     }
 }
--- source/R5T.D0078.X0001/Code/Services/Extensions/IVisualStudioSolutionFileOperatorExtensions.cs
+++ source/R5T.D0078.X0001/Code/Services/Extensions/IVisualStudioSolutionFileOperatorExtensions.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
+using R5T.Lombardy;
+
 using R5T.D0078;
+using R5T.T0114;
 
 using Instances = R5T.D0078.X0001.Instances;
 
@@ -15,7 +19,17 @@ namespace System
             string solutionFilePathToModify,
             string dependencyProjectReferenceFilePathToAdd)
         {
-            return visualStudioSolutionFileOperator.AddProjectReferences(
+            return visualStudioSolutionFileOperator.AddProjectReference(
+                solutionFilePathToModify,
+                dependencyProjectReferenceFilePathToAdd,
+                Instances.SolutionFolderName.Dependencies());
+        }
+
+        public static Task AddDependencyProjectReferenceOkIfAlreadyAdded(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePathToModify,
+            string dependencyProjectReferenceFilePathToAdd)
+        {
+            return visualStudioSolutionFileOperator.AddProjectReferenceOkIfAlreadyAdded(
                 solutionFilePathToModify,
                 dependencyProjectReferenceFilePathToAdd,
                 Instances.SolutionFolderName.Dependencies());
@@ -29,6 +43,130 @@ namespace System
                 solutionFilePathToModify,
                 dependencyProjectReferenceFilePathsToAdd,
                 Instances.SolutionFolderName.Dependencies());
+        }
+
+        public static async Task AddDependencyProjectReferencesOfProjectReferences(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePath,
+            params IProjectFileSpecification[] projectFileSpecifications)
+        {
+            var dependencyProjectReferenceFilePaths = projectFileSpecifications
+                .SelectMany(xProjectFileSpecification => xProjectFileSpecification.DependencyProjectReferenceFilePaths)
+                .Distinct()
+                .OrderAlphabetically()
+                .ToArray();
+
+            foreach (var dependencyProjectReferenceFilePath in dependencyProjectReferenceFilePaths)
+            {
+                await visualStudioSolutionFileOperator.AddDependencyProjectReference(
+                    solutionFilePath,
+                    dependencyProjectReferenceFilePath);
+            }
+        }
+
+        /// <summary>
+        /// Adds only the project reference file paths of the input <see cref="ProjectFileSpecification"/>s.
+        /// </summary>
+        public static async Task AddProjectReferencesOfProjectReferencesOnly(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePath,
+            params IProjectFileSpecification[] projectFileSpecifications)
+        {
+            var projectReferenceFilePaths = projectFileSpecifications
+                .SelectMany(xProjectFileSpecification => xProjectFileSpecification.ProjectReferenceFilePaths)
+                .Distinct()
+                .OrderAlphabetically()
+                .ToArray();
+
+            foreach (var projectReferenceFilePath in projectReferenceFilePaths)
+            {
+                await visualStudioSolutionFileOperator.AddProjectReference(
+                    solutionFilePath,
+                    projectReferenceFilePath);
+            }
+        }
+
+        /// <summary>
+        /// Adds both the dependency project reference file paths and project reference file paths of the input <see cref="ProjectFileSpecification"/>.
+        /// </summary>
+        public static async Task AddProjectReferencesOfProjectReferences(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePath,
+            params IProjectFileSpecification[] projectFileSpecifications)
+        {
+            await visualStudioSolutionFileOperator.AddProjectReferencesOfProjectReferencesOnly(
+                solutionFilePath,
+                projectFileSpecifications);
+
+            await visualStudioSolutionFileOperator.AddDependencyProjectReferencesOfProjectReferences(
+                solutionFilePath,
+                projectFileSpecifications);
+        }
+
+        public static async Task CreateOnlyIfNotExists(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePath)
+        {
+            var solutionFileExists = Instances.FileSystemOperator.FileExists(solutionFilePath);
+
+            if (solutionFileExists)
+            {
+                return;
+            }
+
+            await visualStudioSolutionFileOperator.Create(solutionFilePath);
+        }
+
+        public static async Task Create(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePath)
+        {
+            var solutionDirectoryPath = Instances.SolutionPathsOperator.GetSolutionDirectoryPath(solutionFilePath);
+
+            var solutionName = Instances.SolutionPathsOperator.GetSolutionName(solutionFilePath);
+
+            await visualStudioSolutionFileOperator.Create(solutionName, solutionDirectoryPath);
+        }
+
+        public static async Task<string[]> ListProjectReferencePaths(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePath,
+            IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            var solutionDirectoryPath = stringlyTypedPathOperator.GetDirectoryPathForFilePath(solutionFilePath);
+
+            var projectReferenceRelativePaths = await visualStudioSolutionFileOperator.ListProjectReferenceRelativePaths(solutionFilePath);
+
+            var output = projectReferenceRelativePaths
+                .Select(xRelativeFilePath => stringlyTypedPathOperator.Combine(solutionDirectoryPath, xRelativeFilePath))
+                .ToArray();
+
+            return output;
+        }
+
+        public static async Task<bool> HasProjectReference(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePath,
+            string projectReferenceFilePath,
+            IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            var projectReferencePaths = await visualStudioSolutionFileOperator.ListProjectReferencePaths(
+                solutionFilePath,
+                stringlyTypedPathOperator);
+
+            var output = projectReferencePaths.Contains(projectReferenceFilePath);
+            return output;
+        }
+
+        public static async Task<bool> RemoveProjectReferenceOkIfNotExists(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator,
+            string solutionFilePath,
+            string projectReferenceFilePath,
+            IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            var hasProjectReference = await visualStudioSolutionFileOperator.HasProjectReference(
+                solutionFilePath,
+                projectReferenceFilePath,
+                stringlyTypedPathOperator);
+
+            if (hasProjectReference)
+            {
+                await visualStudioSolutionFileOperator.RemoveProjectReferenceIdempotent(solutionFilePath, projectReferenceFilePath);
+            }
+
+            return hasProjectReference;
         }
     }
 }
